4e924a604b8908a5fa294e1ffd7f076c08040426
MD ABU SAYED <62033575+royalcourtbd@users.noreply.github.com>
2022-08-15 00:36:31 +0600

    eta ki

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       initialBinding: AllControllerBinding(),
-      home: MainScreenPageView(),
+      home: const MainScreenPageView(),
     );
   }
 }
